d208c08ba9a5c2167c7189782f174092fa717c48
redauthorium <aaliyaaousama@gmail.com>
2022-04-28 16:54:16 +0700

    setting warna dan ikon map secara dinamis di sewa aset, potensi aset dan aset tanah bersertifikat

--- public/partials/wp-aset-bmd-detail-aset-jalan.php
+++ public/partials/wp-aset-bmd-detail-aset-jalan.php
@@ -1,5 +1,3 @@
-            <?php echo $ikon_map; ?>
-            <?php echo 'warna maphaha'; ?>
             <div class="form-group row">
                 <label class="col-md-2 col-form-label">Lokasi</label>
                 <div class="col-md-10">
