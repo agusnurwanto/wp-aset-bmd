e8159d1ad39c149047b7ccad05aed85dae7a0866
agusnurwanto <agusnurwantomuslim@gmail.com>
2022-05-31 11:37:36 +0700

    fix fitur kotak pencarian alamat google maps

--- public/partials/wp-aset-bmd-detail-aset-jalan.php
+++ public/partials/wp-aset-bmd-detail-aset-jalan.php
@@ -268,6 +268,40 @@
     }
 <?php endif; ?>
 
+<?php if(!empty($allow_edit_post) && !empty($params['key']['edit'])): ?>
+    function cari_alamat() {
+        var alamat = jQuery('#cari-alamat-input').val();
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { 'address': alamat}, function(results, status) {
+            if (status == 'OK') {
+                console.log('results', results);
+                map.setCenter(results[0].geometry.location);
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+    }
+
+    jQuery(document).ready(function(){
+        var search = ''
+            +'<div class="input-group" style="margin-bottom: 5px; display: block;">'
+                +'<div class="input-group-prepend">'
+                    +'<input class="form-control" id="cari-alamat-input" type="text" placeholder="Kotak pencarian alamat">'
+                    +'<button class="btn btn-success" id="cari-alamat" type="button"><i class="dashicons dashicons-search"></i></button>'
+                +'</div>'
+            +'</div>';
+        jQuery("#map-canvas").before(search);
+        jQuery("#cari-alamat").on('click', function(){
+            cari_alamat();
+        });
+        jQuery("#cari-alamat-input").on('keyup', function (e) {
+            if (e.key === 'Enter' || e.keyCode === 13) {
+                cari_alamat();
+            }
+        });
+    });
+<?php endif; ?>
+
     var map;
     var nama_aset;
     var kode_aset;
