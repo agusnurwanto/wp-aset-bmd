a62a2766945b95fedb69796977e7eaf165eb211c
agusnurwanto <agusnurwantomuslim@gmail.com>
2022-06-02 17:51:52 +0700

    perbaikan update status post aset belum masuk neraca

--- public/partials/wp-aset-bmd-belum-masuk-neraca.php
+++ public/partials/wp-aset-bmd-belum-masuk-neraca.php
@@ -36,13 +36,15 @@ if(is_user_logged_in()){
 }
 
 $args = array(
+    'post_type' => 'post',
+    'post_status' => 'any',
     'posts_per_page' => -1,
     'meta_query' => array(
-       array(
-           'key' => 'abm_kd_register',
-           'value' => array(''),
-           'compare' => 'NOT IN',
-       )
+        array(
+            'key' => 'abm_kd_register',
+            'value' => array(''),
+            'compare' => 'NOT IN',
+        )
    )
 );
 $query = new WP_Query($args);
@@ -125,6 +127,10 @@ foreach($query->posts as $post){
         array(
             'key' =>'detail',
             'value' => 1
+        ),
+        array(
+            'key' =>'skip',
+            'value' => 1
         )
     );
     $link_detail = $this->functions->get_link_post($post);
@@ -134,6 +140,10 @@ foreach($query->posts as $post){
         $post->custom_url = array(
             array(
                 'key' =>'edit',
+                'value' => 1
+            ),
+            array(
+                'key' =>'skip',
                 'value' => 1
             )
         );
@@ -156,7 +166,7 @@ foreach($query->posts as $post){
         <tr>
             <td class="text-center">'.$kd_upb.'<br>'.$abm_nama_upb.'</td>
             <td class="text-center">'.$data_jenis['nama'].'</td>
-            <td class="text-center">'.$abm_kd_barang.'.'.$abm_kd_register.'</td>
+            <td class="text-center">'.$abm_kd_barang.'.'.$this->functions->CekNull($abm_kd_register, 6).'</td>
             <td>'.$nama_aset.'</td>
             <td>'.$column_lokasi.'</td>
             <td>'.$keterangan.'</td>
